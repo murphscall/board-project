07f88194373a8b1a481ec23118a4a2c0a77a1110
kimjinhoo <wlsgnwkd22@naver.com>
2025-08-08 12:48:49 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/kimje/springboard/MainController.java
@@ -0,0 +1,13 @@
+package com.kimje.springboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String MainPage() {
+        return "home";
+    }
+}
